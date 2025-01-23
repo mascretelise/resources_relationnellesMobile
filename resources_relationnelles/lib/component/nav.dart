708ddf38f35bc2e4c7e10351deb6835e7fa10288
RICHARD Quentin <richard.quentin.t@gmail.com>
2025-01-23 16:28:26 +0100

    Changement du fonctionnement de l'affichage de la page paramètres, changement de la navbar pour qu'elle soit affichée en haut

--- resources_relationnelles/lib/component/nav.dart
+++ resources_relationnelles/lib/component/nav.dart
@@ -1,60 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/Pages/accueil.dart';
 import 'package:myapp/Pages/login.dart';
+import 'package:myapp/Pages/settings.dart';
 
-class Nav extends StatefulWidget {
+class Nav extends StatelessWidget {
   const Nav({super.key});
 
   @override
-  State<Nav> createState() => _NavState();
-}
-
-class _NavState extends State<Nav> {
-  int _selectedIndex = 0;
-  final List<Widget> _widgetOptions = <Widget>[
-    Accueil(),
-    Text(
-        'Paramètres'), //Change le texte en paramètre à l'appui du bouton à l'index 2.
-    Login(), //Affiche la page test.dart à l'appui du bouton à l'index 3.
-  ];
-
-  void _onItemTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: CircleAvatar(
-          backgroundImage: AssetImage('assets/images/logo.png'),
-          radius: 25,
+    return DefaultTabController(
+      length: 3, // Nombre d'onglets
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('(Re)Sources Relationnelles'), //Nom de l'appli
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/images/logo.png'),
+                radius: 20,
+              ),
+            ],
+          ),
+          backgroundColor: Colors.blue[100],
+          bottom: const TabBar(
+            tabs: [
+              //Définis les icones et le nom des boutons de navigation
+              Tab(icon: Icon(Icons.account_circle_outlined), text: 'Compte'),
+              Tab(icon: Icon(Icons.settings), text: 'Paramètres'),
+              Tab(icon: Icon(Icons.login), text: 'Connexion'),
+            ],
+          ),
         ),
-        backgroundColor: Colors.blue[100],
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Compte',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Paramètres',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Connexion',
-          )
-        ],
-        backgroundColor: Colors.blue[100],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTap,
+        body: const TabBarView(
+          children: [
+            //Instanciation des classes définies dans le répertoire des pages dans l'ordre des tabs
+            Accueil(),
+            Settings(),
+            Login(),
+          ],
+        ),
       ),
     );
   }
